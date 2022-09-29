14b2110293959c238e33fb8cc79f7406ee673778
progietheus <crypxtic@gmail.com>
2022-09-29 07:24:31 -0700

    first commit

---
+++ 1_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Hello, world \d");
+
+    //Providing a different escape sequence than the ones provided (\t, \b, \n, \\, \")
+    //results in a warning from compiler that identifes it as an unknown escape sequence
+}
---
+++ 1_3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        //celsius = 5 * (fahr -32) / 9;
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }    
+
+    /* C also provides other basic data types like:
+        
+        char
+        short
+        long
+        double
+
+    */
+}
